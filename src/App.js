9d329c6a5e38e2948be2d3572ccc96805149eae0
daniel <hdanielmm@gmail.com>
2019-10-23 13:04:01 +0200

    Add form and button to create tasks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "",
+    description: "",
+    check: false,
+    tasks: []
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <label htmlFor="name">Task name</label>
+
+          <input
+            id="name"
+            name="name"
+            value={this.state.name}
+            onChange={this.handlerChange}>
+          </input>
+
+          <label htmlFor="description">Description</label>
+
+          <input
+            id="description"
+            name="description"
+            value={this.state.description}
+            onChange={this.handlerChange}>
+          </input>
+
+          <label htmlFor="check">Check</label>
+
+          <input type="checkbox" id="check" name="check"
+            onChange={this.handlerChange}>
+          </input>
+
+          <button onClick={this.handlerSubmit}>Create task</button>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>State</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.tasks.map(e => (
+              <tr>
+                <td>{e.name}</td>
+                <td>{e.description}</td>
+                <td>{e.check}</td>
+              </tr>
+            )
+            )}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  handlerSubmit = (e) => {
+    e.preventDefault();
+
+    const newTask = {
+      name: this.state.name,
+      description: this.state.description,
+      check: this.state.check ? "Done" : "To do"
+    }
+
+    this.setState({
+      tasks: this.state.tasks.concat(newTask)
+    })
+    console.log(this.state)
+  }
+
+  handlerChange = (e) => {
+    const type = e.currentTarget.type;
+    const value = type === "checkbox" ? e.currentTarget.checked : e.currentTarget.value;
+    const name = e.currentTarget.name;
+
+    this.setState({
+      [name]: value,
+    })
+
+  }
+
+  
 }
 
 export default App;
